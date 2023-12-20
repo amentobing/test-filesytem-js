b891828b54012d68bc7abf048837a059b8aab2d2
Amen Tobing <amentobing11@gmail.com>
2023-12-20 10:04:35 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
+  res.send("Server on working!");
+});
+
+app.use("/test", (req, res) => {
   res.download(__dirname + "/files/1-KONSEP-DASAR-PYTHON.docx");
 });
 
